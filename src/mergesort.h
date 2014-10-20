5bb63ead899a577c8dba432c6ee3f8b2d046e4f2
Prashant Solanki <Sanki@prashant.solanki@outlook.in>
2014-10-20 04:32:58 -0400

    Adding code for parallel merge sort

--- src/mergesort.h
+++ src/mergesort.h
@@ -1,42 +1,43 @@
 #ifndef __MERGE_SORT_H__
 #define __MERGE_SORT_H__
-
+#include <debug_print.h>
 namespace MergeSort
 {
   template <typename Type>
-  void sortArray(Type *arr, int size)
-  {
-    if(size > 1)
-    { // Divide
-      sortArray( arr, size/2 );
-      sortArray( &arr[size/2], size/2 );
-    }
-    else
-    { // Conquer
-      return;
-    }
-    // Merge
-    int k = size/2;
-    for(int i=0; i<size; i++)
+    void sortArray(Type *arr, int size)
     {
-      if(arr[i] < arr[k] && i<k )
+#pragma omp parallel
       {
-      }
-      else if(k<size)
-      {
-        int l=k;
-        while(l>i)
+#pragma omp single nowait
         {
-          Type t = arr[l];
-          arr[l] = arr[l-1];
-          arr[l-1] = t;
-          l--;
+          if(size > 1)
+          { // Divide
+#pragma omp task
+            sortArray( arr, size/2 );
+#pragma omp task
+            sortArray( &arr[size/2], size-(size/2) );
+#pragma omp taskwait
+            // Merge
+            int k = size/2;
+            for(int i=0; i<size; i++)
+            {
+              if(!(arr[i] < arr[k] && i<k ) && k<size)
+              {
+                int l=k;
+                while(l>i)
+                {
+                  Type t = arr[l];
+                  arr[l] = arr[l-1];
+                  arr[l-1] = t;
+                  l--;
+                }
+                k++;
+              }
+            }
+          }
         }
-        k++;
       }
     }
-  }
-
 };
 
 #endif //__MERGE_SORT_H__
