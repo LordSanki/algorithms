71e1542b655b471764818f101326b77f0cbcebf1
Prashant Solanki <Sanki@prashant.solanki@outlook.in>
2014-10-20 06:34:10 -0400

    fixing bug condition was missed out adding function to count inversions

--- src/mergesort.h
+++ src/mergesort.h
@@ -21,7 +21,7 @@ namespace MergeSort
             int k = size/2;
             for(int i=0; i<size; i++)
             {
-              if((arr[i] > arr[k]) && (k<size) )
+              if((arr[i] > arr[k]) && (k<size) && (i<k))
               {
                 Type t = arr[k];
                 int l=k;
@@ -38,6 +38,37 @@ namespace MergeSort
         }
       }
     }
+
+  template <typename Type>
+    unsigned long int countInversions(Type *arr, int size)
+    {
+      unsigned long int count=0;
+      if(size > 1)
+      { // Divide
+        count += countInversions( arr, size/2 );
+        count += countInversions( &arr[size/2], size-(size/2) );
+        // Merge
+        int k = size/2;
+        for(int i=0; i<size; i++)
+        {
+          if((arr[i] > arr[k]) && (k<size) && (i<k))
+          {
+            Type t = arr[k];
+            int l=k;
+            while(l>i)
+            {
+              arr[l] = arr[l-1];
+              l--;
+              count++;
+            }
+            arr[i] = t;
+            k++;
+          }
+        }
+      }
+      return count;
+    }
+
 };
 
 #endif //__MERGE_SORT_H__
